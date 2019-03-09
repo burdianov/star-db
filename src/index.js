b959c2af9d8664c0c4fc30066b28f29c40ea7aba
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-03-09 17:42:10 +0400

    grab data from server using fetch API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello React!");
+const getResource = async url => {
+  const res = await fetch(url);
+  const body = await res.json();
+  return body;
+};
+
+getResource("https://swapi.co/api/people/1/").then(body => {
+  console.log(body);
+});
