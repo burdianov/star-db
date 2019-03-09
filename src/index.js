bb76fed82a60dd6c1a2f4aadc60ace754912f394
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-03-09 20:41:55 +0400

    processing the errors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 const getResource = async url => {
   const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}, received ${res.status}`);
+  }
+
   const body = await res.json();
   return body;
 };
 
-getResource("https://swapi.co/api/people/1/").then(body => {
-  console.log(body);
-});
+getResource("https://swapi.co/api/people/1/")
+  .then(body => {
+    console.log(body);
+  })
+  .catch(err => {
+    console.error("Could not fetch", err);
+  });
